33bd120da910fe092305b781a7a04f39bdf8aafe
raulshma <raulshma@gmail.com>
2022-10-05 23:44:17 +0530

    updated vite base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-formik-fieldarray-multicomponent/',
+  base: '/react-forms-fieldarray-multicomponent/',
   plugins: [react(), eslint()],
 });
